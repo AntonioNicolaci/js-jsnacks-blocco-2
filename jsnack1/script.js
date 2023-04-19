7a85fec016244a53fc9580f04078e612202e982f
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-19 10:00:40 +0200

    fix array in to empty array

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,8 @@
-let numeriArray = ["", "", "", "", "", ""];
+let numeriArray = [];
 
 function numeri(){
     let casellaArray = 0;
-    for(let i = 0; numeriArray.length > i; i++) {
+    for(let i = 0; 6 > i; i++) {
         let appoggio = prompt("Scrivi il primo numero");
         if([appoggio % 2] != 0){
             numeriArray[casellaArray] = appoggio;
