154be6a48e2507ee80ee3b78d1e614b0f0a2e397
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-19 10:45:11 +0200

    fix lenght list number in html page

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,8 +1,20 @@
 const nomeArray = ["Achille", "Alissa", "Antonio", "Dora", "Delia", "Franco", "Franz", "Josè", "Niccolo'", "Tabita"];
 const cognomeArray = ["Rossi", "Galli", "Ferri", "Fermi", "Rizzi", "Longo", "Polidoro", "Greco", "Martini", "Di Stefano"]; //10 cognomi da fare
+let listaInvitati = [];
 
 function randomName(){
-    let numeroNome = Math.floor(Math.random()*nomeArray.length);
-    let numeroCognome = Math.floor(Math.random()*nomeArray.length);
-    document.getElementById("nome_invitato").innerHTML = `Il nome dell'invitato è ${nomeArray[numeroNome]} ${cognomeArray[numeroCognome]}`
+
+    let numeroNome, numeroCognome, divInvitati = "";
+    const lunghezzaLista = Math.floor(Math.random()*20);
+
+    for(let i = 0; lunghezzaLista > i; i++){
+        numeroNome =  Math.floor(Math.random()*nomeArray.length);
+        numeroCognome =  Math.floor(Math.random()*cognomeArray.length);
+        listaInvitati[i] = `${nomeArray[numeroNome]} ${cognomeArray[numeroCognome]}`;
+    }
+
+    for(let i = 0; listaInvitati.length > i; i++){
+        divInvitati += `<span>Il nome dell'invitato è ${listaInvitati[i]}</span>`;
+    }
+    document.getElementById("lista_invitati").innerHTML = divInvitati;
 }
